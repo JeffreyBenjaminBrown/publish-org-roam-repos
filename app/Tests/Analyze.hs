3ded5e6d4941f0ddb190b0515a2abc82c2e9a8c6
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-09-08 14:42:48 -0500

    another test

--- app/Tests/Analyze.hs
+++ app/Tests/Analyze.hs
@@ -9,6 +9,7 @@ allTests :: Test
 allTests = TestList
   [ test_is_properties
   , test_id_if_id
+  , test_is_title
   ]
 
 test_is_properties :: Test
@@ -23,3 +24,9 @@ test_id_if_id :: Test
 test_id_if_id = TestCase $ do
   assertBool "" $ id_if_id "  :ID: 123  " == Just "123"
   assertBool "" $ id_if_id "  123  :ID:  " == Nothing
+
+test_is_title :: Test
+test_is_title = TestCase $ do
+  assertBool "" $       is_title "#+title: science"
+  assertBool "" $       is_title "  #+title:    science is fun  "
+  assertBool "" $ not $ is_title "#+  title: science" -- the space after the `+` disqualifies it
