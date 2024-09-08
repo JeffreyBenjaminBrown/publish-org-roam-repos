93173396a5c8bb3945737f65f01729c5f6210929
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-09-08 14:54:02 -0500

    more tests
      all of Analyze is now tested

--- app/Tests/Analyze.hs
+++ app/Tests/Analyze.hs
@@ -10,6 +10,7 @@ allTests = TestList
   [ test_is_properties
   , test_id_if_id
   , test_is_title
+  , test_countTitleLine
   ]
 
 test_is_properties :: Test
@@ -30,3 +31,36 @@ test_is_title = TestCase $ do
   assertBool "" $       is_title "#+title: science"
   assertBool "" $       is_title "  #+title:    science is fun  "
   assertBool "" $ not $ is_title "#+  title: science" -- the space after the `+` disqualifies it
+
+test_countTitleLine :: Test
+test_countTitleLine = TestCase $ do
+  assertBool "" $
+    countTitleLine [ "whatever",
+                     "line 2 isn't it either",
+                     "#+title: science" ]
+    == Just 3
+  assertBool "" $
+    countTitleLine [ "there isn't one",
+                     "in this file" ]
+    == Nothing
+
+test_linesOfIds :: Test
+test_linesOfIds = TestCase $ do
+  let file =
+        [ ":PROPERTIES:"
+        , ":ID:       111"
+        , ":END:"
+        , "#+title: science"
+        , "* [[id:c35ab968-7056-40fa-8816-ea16d5c88f6d][a list of sciences]]"
+        , "* some text, whatever"
+        , "** lalala"
+        , "* an apparently important header"
+        , "  :PROPERTIES:"
+        , "  :ID:       222" -- the 10th line (1-indexed)
+        , "  :END:"
+        , "* even more text"
+        , "** lalala" ]
+      Just titleLine = countTitleLine file
+  assertBool "" $ linesOfIds titleLine file
+    == [ ("111", Nothing),
+         ("222", Just 10) ]
